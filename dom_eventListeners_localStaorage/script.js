f16d27976fe3ef9e0d37129e9a25dba65b54e579
And4Web <anands2807@gmail.com>
2022-06-08 15:43:45 +0530

    dom and various dom properties

---
+++ dom_eventListeners_localStaorage/script.js
@@ -0,0 +1,18 @@
+// console.log(document);
+//document.all
+//document.all[10]
+//document.body
+//document.head
+//document.URL
+//document.location;
+// document.charset;
+// document.children;
+// document.childNodes;
+// document.doctype;
+//document.links
+//document.links[2]
+//document.links[2].id
+//document.links[2].className
+//document.links[2].classList
+
+// console.log(document.scripts[0].getAttribute("src"));
